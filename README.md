c9910171537c2d3cc7df62768d0c9d0b58870cbc
William King <williamking9772@gmail.com>
2021-03-23 16:56:59 +0000

    filled README and completed if, elif, else task

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
 # Python Control Flow
-## if, else if - elif and else
-## for loops
+## Conditional Statements
+Conditional statements change the control flow of the program.
+ * `if` -  runs a piece of code if the condition is met.
+ * `elif` - same as if, but it is checked if the previous
+            conditions are not met.
+ * `else` - runs if none of the other conditions are met.
+
+```python
+age = 18
+if age >= 18:
+    print("You are an adult.")
+elif age < 18:
+    print("You are not an adult.")
+else:
+    print("Oops, something went wrong.")
+```
+
 ## while loops
+While loops execute a section of code until the condition is 
+no longer met. Without a stopping condition, while loops will
+run indefinitely.
+```python
+# Counts down from 10 to 1, and outputs it.
+num = 10
+while num > 0:
+    print(num)
+    num -= 1
+```
+
+## for loops
+For loops are used to iterate over a sequence (data collections, 
+strings). They can be modified to iterate a section of code a
+set number of times using `range()`.
+
+Iterating through data collections
+```python
+# Iterating through a list
+fruits = ["apple", "coconut", "grapes"]
+for x in fruits:
+    print(fruits)
+    
+# Iterating a string. Prints each letter.
+for x in "king":
+    print(x)
+```
+
+Using `range()`
+```python
+# Prints numbers 0 to 9
+for i in range(10):
+    print(i)
+```
---
+++ control_flow.py
@@ -0,0 +1,59 @@
+#### Control Flow ####
+# if, elif, else
+# Conditional statements are used to control the flow of the program
+# age = 14
+# # if the user is over 15, allow them to buy a ticket
+# if age >= 15: # If this condition is met, the print statement will execute
+#     print("Thank you, please proceed to your purchase.")
+#
+# # If the user is under 15, do not allow them to buy a ticket
+# elif age < 15:
+#     print("Sorry, you are too young to watch this movie.")
+#
+# # Else block executes if none of the above conditions are met
+# else:
+#     print("Oops, something went wrong, please try later.")
+
+#### Exercise ####
+# As a user, I would like to sell tickets according to the age of the user
+# and the category of the movie age = U, PG, 12, 12a, 15, 18
+# user input to let us know the age to decide whether to sell the ticket or not
+# casting implemented int()
+# display the age back to the user with an appropriate message
+
+movies = {
+    # film : age
+    "Predator": 18,
+    "The Thing": 18,
+    "Ip Man": 15,
+    "Jaws": 12,
+    "Monster House": 8, # Apparently, PG is an age rating of 8
+    "Chicken Run": 0,
+    "Flushed Away": 0,
+    "The Lorax": 0
+}
+
+# Convert movie keys into a set to randomise it.
+movies_set = set(movies.keys())
+# Sets can be accessed, so it is converted to a list.
+movie_chosen = list(movies_set)[3]
+# Fetch the chosen movie's age.
+movie_age = movies[movie_chosen]
+
+print(f"The movie screened now is {movie_chosen}.")
+my_age = int(input("Please enter your age: "))
+print(f"You are {my_age} years old.") # print the age.
+
+if my_age >= movie_age:
+    print("Thank you, please proceed to your purchase.")
+elif my_age < movie_age:
+    print("Sorry, you are too young to watch this movie.")
+else:
+    print("Oops, something went wrong.")
+
+
+#### WHILE LOOPS ####
+
+
+#### FOR LOOPS ####
+
